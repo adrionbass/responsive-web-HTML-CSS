34333b3de391d2167c64e9458311660f3339c94f
Adriel Miño <adrielsmb@gmail.com>
2023-02-14 00:45:31 -0300

    13/2/2023B

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+/* HAMB BTN */
+
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("mobile-menu");
+});
+
+
+/* SCROLL UP */
+
+upBttn = document.getElementById("up-btn");
+upBttn.addEventListener("click", scrollUp);
+
+function scrollUp() {
+  var currentScroll =
+    document.documentElement.scrollTop || document.body.scrollTop;
+  if (currentScroll > 0) {
+    window.requestAnimationFrame(scrollUp);
+    window.scrollTo(0, currentScroll - currentScroll / 10);
+    upBttn.style.transform = "scale(0)";
+  }
+}
+
+window.onscroll = function () {
+  var scroll = document.documentElement.scrollTop;
+  if (scroll > 500) {
+    upBttn.style.transform = "scale(1)";
+  } else if (scroll < 500) {
+    upBttn.style.transform = "scale(0)";
+  }
+};
